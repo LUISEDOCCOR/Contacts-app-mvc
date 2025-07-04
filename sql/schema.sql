555eff6646b28263fc4870dcf070734473a0b105
Luis Eduardo Ocegueda <luis@eduardoocegueda@gmail.com>
2025-07-04 11:50:04 -0600

    fix: create a database for use in development

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,11 +1,12 @@
--- Active: 1749252434297@@127.0.0.1@3306@app_contactos
-CREATE DATABASE app_contactos;
+-- Active: 1749252434297@@127.0.0.1@3306@app_contactos_develop
+CREATE DATABASE app_contactos_develop;
 USE app_contactos;
-CREATE TABLE TABLE IF NOT EXISTS usuarios (
+DROP TABLE usuarios;
+CREATE TABLE IF NOT EXISTS usuarios (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(255),
     correo VARCHAR(255),
-    password VARCHAR(255),
+    password VARCHAR(255)
 );
 CREATE TABLE IF NOT EXISTS contactos (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -13,5 +14,5 @@ CREATE TABLE IF NOT EXISTS contactos (
     numero VARCHAR(255),
     usuario_id INT,
 
-    FOREIGN KEY usuario_id REFERENCES usuarios(id)
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 );
